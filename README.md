cd4bc33ee9c53b76d19661a46c7db78673f08769
Vasily Dik <vasily.dik.a@gmail.com>
2024-10-03 21:47:51 +0300

    finished landing

---
+++ README.md
@@ -0,0 +1,14 @@
+## Описание
+
+Проект интернет магазина мебели
+
+## Использованные знания
+- HTML5;
+- CSS3:
+  - Flexbox;
+  - Adaptive UI;
+  - Media Queries;
+- JavaScript;
+- React;
+- Webpack;
+- Git.
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Footer.css';
+
+export function Footer() {
+    return (
+        <footer id="contacts">
+            <div>
+                <h3>House staff</h3>
+                <span>
+                    Благодаря нам вы легко сможете оформить свой интерьер в
+                    стиле джапанди
+                </span>
+            </div>
+            <div>
+                <h3>Служба заботы</h3>
+                <div>Контактный телефон: +77593768265</div>
+                <div>Наш email: furniture@sap.com</div>
+            </div>
+        </footer>
+    );
+}
